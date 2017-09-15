213ef169a97c8eef6d90d0c6adeecc6acbe2655f
Alexey Shvedov <a.shvedov@edgency.ru>
2017-09-15 15:20:40 +0300

    just change

--- TestAppGit/DataViewController.swift
+++ TestAppGit/DataViewController.swift
@@ -17,6 +17,9 @@ class DataViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //file changed by this comment
     }
 
     override func didReceiveMemoryWarning() {
